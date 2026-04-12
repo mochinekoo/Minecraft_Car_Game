820d4d6579003cd42486aa5b31d3d5130dd6802b
mochinekoo <mochinekoo0113@gmail.com>
2026-04-12 19:24:34 +0900

    [add] ゲームを開始できるようになりました。

---
+++ src/main/java/net/mochinekoserver/mc_game_template/manager/GameManager.java
@@ -0,0 +1,96 @@
+package net.mochinekoserver.mc_game_template.manager;
+
+import net.mochinekoserver.mc_game_template.Main;
+import net.mochinekoserver.mc_game_template.status.GameStatus;
+import net.mochinekoserver.mc_game_template.util.PluginUtil;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import javax.annotation.Nonnull;
+
+public class GameManager extends GameBase {
+
+    private static GameManager instance = null;
+
+    public GameManager() {
+        super("Game");
+    }
+
+    public static GameManager getInstance() {
+        if (instance == null) {
+            instance = new GameManager();
+        }
+        return instance;
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void reset() {
+        this.mainTask.cancel();
+        this.mainTask = null;
+        this.time = 0;
+    }
+
+    @Override
+    public int start() {
+        if (status != GameStatus.WAITING) return -1;
+        this.mainTask = new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (status == GameStatus.WAITING || status == GameStatus.COUNTING) {
+                    updateWaitCountScene();
+                }
+                else if (status == GameStatus.RUNNING) {
+                    updateRunningScene();
+                }
+                else if (status == GameStatus.ENDING) {
+                    updateEndingScene();
+                }
+            }
+        }.runTaskTimer(Main.getPlugin(Main.class), 0L, 20L);
+
+        return 0;
+    }
+
+    private void updateWaitCountScene() {
+        if (status != GameStatus.WAITING && status != GameStatus.COUNTING) return;
+        if (countTime <= 0) { //0秒以下
+            //ゲーム開始の処理
+            PluginUtil.sendGlobalInfoMessage("ゲーム開始!");
+            status = GameStatus.RUNNING;
+        }
+        else {
+            //カウントの処理
+            status = GameStatus.COUNTING;
+            PluginUtil.sendGlobalInfoMessage("ゲーム開始まであと" + countTime + "秒です。");
+            countTime--;
+        }
+    }
+
+    private void updateRunningScene() {
+        if (status != GameStatus.RUNNING) return;
+        time--;
+        if (time <= 0) { //ゲーム時間が0秒以下になった場合
+            status = GameStatus.ENDING;
+        }
+    }
+
+    private void updateEndingScene() {
+        if (status != GameStatus.ENDING) return;
+        PluginUtil.sendGlobalInfoMessage("ゲーム終了!");
+        reset();
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public boolean canStart() {
+        return true;
+    }
+}
---
+++ src/main/java/net/mochinekoserver/mc_game_template/status/GameStatus.java
@@ -0,0 +1,10 @@
+package net.mochinekoserver.mc_game_template.status;
+
+public enum GameStatus {
+    WAITING,
+    COUNTING,
+    RUNNING,
+    ENDING;
+
+    GameStatus() {}
+}
