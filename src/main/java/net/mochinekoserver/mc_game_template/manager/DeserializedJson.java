0a0f7b4c0adfc6b8063a458728d2856a39831223
mochinekoo <mochinekoo0113@gmail.com>
2026-04-15 07:51:38 +0900

    [add] JsonManagerを追加

---
+++ src/main/java/net/mochinekoserver/mc_game_template/manager/DeserializedJson.java
@@ -0,0 +1,4 @@
+package net.mochinekoserver.mc_game_template.manager;
+
+public abstract class DeserializedJson {
+}
---
+++ src/main/java/net/mochinekoserver/mc_game_template/status/FileType.java
@@ -0,0 +1,25 @@
+package net.mochinekoserver.mc_game_template.status;
+
+import net.mochinekoserver.mc_game_template.manager.DeserializedJson;
+
+public enum FileType {
+
+    ;
+
+    private final String name;
+    private final Class<? extends DeserializedJson> deserialized_class;
+
+    FileType(String name, Class<? extends DeserializedJson> deserialized_class) {
+        this.name = name;
+        this.deserialized_class = deserialized_class;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Class<? extends DeserializedJson> getDeserializedClass() {
+        return deserialized_class;
+    }
+
+}
