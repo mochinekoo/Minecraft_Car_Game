136914c37331c866dcfe9320c19c6f4d68daa08c
mochinekoo <mochinekoo0113@gmail.com>
2026-04-14 21:19:07 +0900

    [add] リスポーンを実装しました

--- src/main/java/net/mochinekoserver/mc_game_template/listener/PlayerDeathListener.java
+++ src/main/java/net/mochinekoserver/mc_game_template/listener/PlayerDeathListener.java
@@ -1,5 +1,9 @@
 package net.mochinekoserver.mc_game_template.listener;
 
+import net.mochinekoserver.mc_game_template.Main;
+import net.mochinekoserver.mc_game_template.manager.GameManager;
+import net.mochinekoserver.mc_game_template.status.GameStatus;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -8,7 +12,14 @@ public class PlayerDeathListener implements Listener {
 
     @EventHandler
     public void onDeath(PlayerDeathEvent event) {
+        var gameManager = GameManager.getInstance();
+        var status = gameManager.getStatus();
+        var player = event.getEntity();
+        var plugin = Main.getPlugin(Main.class);
 
+        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+            player.spigot().respawn();
+        }, 5L);
     }
 
 }
--- src/main/java/net/mochinekoserver/mc_game_template/listener/PlayerRespawnListener.java
+++ src/main/java/net/mochinekoserver/mc_game_template/listener/PlayerRespawnListener.java
@@ -1,13 +1,55 @@
 package net.mochinekoserver.mc_game_template.listener;
 
+import net.mochinekoserver.mc_game_template.Main;
+import net.mochinekoserver.mc_game_template.manager.ConfigManager;
+import net.mochinekoserver.mc_game_template.manager.GameManager;
+import net.mochinekoserver.mc_game_template.manager.TeamManager;
+import net.mochinekoserver.mc_game_template.status.GameStatus;
+import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class PlayerRespawnListener implements Listener {
 
     @EventHandler
     public void onRespawn(PlayerRespawnEvent event) {
+        var player = event.getPlayer();
+        var gameManager = GameManager.getInstance();
+        var status = gameManager.getStatus();
+        var teamManager = TeamManager.getInstance();
+        var joinTeam = teamManager.getJoinGameTeam(player);
+        var configManager = ConfigManager.getInstance();
+        var lobby = configManager.getLobby();
+
+        if (status == GameStatus.RUNNING) {
+            if (joinTeam == null) {
+                event.setRespawnLocation(lobby); //runningで、参加チームがない場合もロビーに飛ばす
+                return;
+            }
+            var teamSpawn = configManager.getTeamSpawnLocation(joinTeam);
+            event.setRespawnLocation(teamSpawn);
+            player.setGameMode(GameMode.SPECTATOR);
+            new BukkitRunnable() {
+                int respawnTime = 3;
+                @Override
+                public void run() {
+                    if (respawnTime <= 0) {
+                        player.teleport(teamSpawn);
+                        player.setGameMode(GameMode.ADVENTURE);
+                        this.cancel();
+                    }
+                    else {
+                        player.sendTitle("リスポーンまであと" + respawnTime + "秒", "");
+                        respawnTime--;
+                    }
+                }
+            }.runTaskTimer(Main.getPlugin(Main.class), 0L, 20L);
+        }
+        else {
+            event.setRespawnLocation(lobby);
+        }
 
     }
 
