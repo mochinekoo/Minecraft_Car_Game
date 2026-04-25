ece20e88a086c7e0919699ee20a0d27f9623aa34
mochinekoo <mochinekoo0113@gmail.com>
2026-04-25 20:56:04 +0900

    [add] 同じチームだとダメージを受けないように追加しました

--- src/main/java/net/mochinekoserver/mc_game_template/listener/EntityDamageListener.java
+++ src/main/java/net/mochinekoserver/mc_game_template/listener/EntityDamageListener.java
@@ -1,6 +1,8 @@
 package net.mochinekoserver.mc_game_template.listener;
 
+import net.mochinekoserver.mc_game_template.manager.TeamManager;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -11,5 +13,17 @@ public class EntityDamageListener implements Listener {
     public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
         var damage = event.getEntity(); //ダメージを受けた人
         var damager = event.getDamager(); //ダメージを与えた人
+        var cause = event.getCause();
+
+        if (damage instanceof Player damagePlayer && damager instanceof Player damagerPlayer) {
+            var teamManager = TeamManager.getInstance();
+            var damageTeam = teamManager.getJoinGameTeam(damagePlayer);
+            var damagerTeam = teamManager.getJoinGameTeam(damagerPlayer);
+            var IsSomeTeam = (damageTeam == damagerTeam);
+            if (IsSomeTeam) {
+                event.setCancelled(true);
+            }
+        }
+
     }
 }
